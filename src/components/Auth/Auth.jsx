ceb9678465a15760184fac32dc96365512950e73
iskender <even10iq@gmail.com>
2023-01-30 13:20:01 +0000

    started authpage

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Auth.css";
 
 const Auth = () => {
-    return <div></div>;
+  return (
+    <div className="container">
+      <div>cybergun</div>
+      <div>
+        <h1>AUTH</h1>
+        <p>LOGIN</p>
+        <Input color="neutral" size="lg" />
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
