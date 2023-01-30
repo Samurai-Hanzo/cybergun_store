3c110780305b2336f1d7949b410313d9dbe8a290
Miroslava Ten <miroslavaten@MacBook-Air-Miroslava.local>
2023-01-30 14:13:37 +0600

    create navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Navbar = () => {
-    return <div></div>;
+  return (
+    <div>
+      <div className="logo-wrapper">
+        <span>cuberpank</span>
+      </div>
+      <div>
+        <select className="select">
+          <option value="small">Small</option>
+          <option value="family">Family Sedan</option>
+          <option value="lux">Luxury</option>
+        </select>
+        <span>Contact Us</span>
+        <span>Support</span>
+      </div>
+      <div>
+        <span>Log In</span>
+        <div className="cart-icon-wrapper">
+          <img
+            className="cart-icon"
+            src="../../../assets/details/shopping-cart.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
