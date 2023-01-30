77af138dacc76d7576b37a7a7c7ba7abb320b9c4
sardor <sardorumarov208@gmail.com>
2023-01-30 13:35:40 +0600

    created and connected all css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ProductCard from "./components/Product/ProductCard";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <ProductCard />
+        </div>
+    );
 }
 
 export default App;
