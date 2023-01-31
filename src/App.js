02e99b23ff8f567c593b00e4465a4bc849d9290d
Miroslava Ten <miroslavaten@MacBook-Air-Miroslava.local>
2023-01-31 11:58:10 +0600

    make headers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import ProductCard from "./components/Product/ProductCard";
+import Auth from "./components/Auth/Auth";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import StoreHeader from "./components/StoreHeader/StoreHeader";
 
 function App() {
   return (
     <div className="App">
+      {/* <Auth /> */}
       <Navbar />
+      <Header />
+      {/* <StoreHeader /> */}
       <ProductCard />
     </div>
   );
