8ca7b94242983ea555ec1d3ad153a6a7910bdc76
iskender <even10iq@gmail.com>
2023-02-03 13:36:07 +0000

    .

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,13 +1,17 @@
+import { Modal } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 // import AdminTable from "../components/AdminTable/AdminTable";
 import AddProduct from "../components/Product/AddProduct";
 
 const AdminPage = () => {
-    return (
-        <div>
-            <AddProduct />
-        </div>
-    );
+  const { modal } = useSelector((store) => store.products);
+  return (
+    <div>
+      <AddProduct />
+      {modal ? <Modal /> : ""}
+    </div>
+  );
 };
 
 export default AdminPage;
