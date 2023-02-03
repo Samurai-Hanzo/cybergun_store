86219ef1d054aa173395484629e0104f24b3443d
Miroslava Ten <miroslavaten@MacBook-Air-Miroslava.local>
2023-02-03 17:20:24 +0600

    add function for cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,65 +1,63 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import CartLogo from "../../assets/details/shopping-cart.png";
 import { useAuth } from "../../contexts/AuthContextProvider";
+import { Button } from "@mui/material";
+import Drawer from "../Drawer/Drawer";
 
 const Navbar = () => {
-    const { user, handleLogOut } = useAuth();
-    return (
-        <div className="container">
-            <div className="navbar-wrapper">
-                <div className="logo-wrapper">
-                    <Link
-                        to="/"
-                        style={{ textDecoration: "none", color: "black" }}
-                    >
-                        <span className="logo">cybergun</span>
-                    </Link>
-                </div>
-                <div className="menu-wrapper">
-                    <Link
-                        to="/products"
-                        style={{ textDecoration: "none", color: "black" }}
-                    >
-                        <span>Store</span>
-                    </Link>
-                    <Link
-                        to="/aboutus"
-                        style={{ textDecoration: "none", color: "black" }}
-                    >
-                        <span>About Us</span>
-                    </Link>
-                    <Link
-                        to="/support"
-                        style={{ textDecoration: "none", color: "black" }}
-                    >
-                        <span>Support</span>
-                    </Link>
-                </div>
-                <div className="login-wrapper">
-                    {user?.email ? (
-                        <span onClick={handleLogOut}>Log out</span>
-                    ) : (
-                        <Link
-                            to="/auth"
-                            style={{ textDecoration: "none", color: "black" }}
-                        >
-                            <span>Log In</span>
-                        </Link>
-                    )}
-
-                    <div className="cart-icon-wrapper">
-                        <Link
-                            to="/cart"
-                            style={{ textDecoration: "none", color: "black" }}
-                        >
-                            <img className="cart-icon" src={CartLogo} alt="" />
-                        </Link>
-                    </div>
-                </div>
-            </div>
+  const [showCart, setShowCart] = useState(false);
+  const { user, handleLogOut } = useAuth();
+  return (
+    <div className="container">
+      {showCart ? <Drawer setShowCart={setShowCart} /> : null}
+      <div className="navbar-wrapper">
+        <div className="logo-wrapper">
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            <span className="logo">cybergun</span>
+          </Link>
         </div>
-    );
+        <div className="menu-wrapper">
+          <Link
+            to="/products"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <span>Store</span>
+          </Link>
+          <Link
+            to="/aboutus"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <span>About Us</span>
+          </Link>
+          <Link
+            to="/support"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <span>Support</span>
+          </Link>
+        </div>
+        <div className="login-wrapper">
+          {user?.email ? (
+            <span onClick={handleLogOut}>Log out</span>
+          ) : (
+            <Link to="/auth" style={{ textDecoration: "none", color: "black" }}>
+              <span>Log In</span>
+            </Link>
+          )}
+
+          <div className="cart-icon-wrapper">
+            <Button
+              onClick={() => setShowCart(true)}
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              <img className="cart-icon" src={CartLogo} alt="" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
